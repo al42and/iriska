a08044a656eefdeac5e55645f796b5a2a15ba5cb
Andrey Alekseenko <al42and@gmail.com>
2017-03-29 00:43:32 +0300

    Minor cleanup

--- js/iriska.js
+++ js/iriska.js
@@ -1,74 +1,79 @@
+/*jslint browser: true*/
+/*global window*/
+
+
 function el(id){return document.getElementById(id);} // Get elem by ID
 
-open_post = function(verb, url, data, target) {
-    var form = document.createElement('form');
+function open_post(verb, url, data, target) {
+    "use strict";
+    var form = document.createElement("form");
     form.action = url;
     form.method = verb;
-    form.enctype = 'multipart/form-data';
-    form.target = target || '_self';
+    form.enctype = "multipart/form-data";
+    form.target = target || "_self";
     if (data) {
-        for (var key in data) {
-            var input = document.createElement('input');
-            //console.log(key + ' - ' + data[key] + ' - ' + JSON.stringify(data[key]));
-            input.type = 'hidden';
+        Object.keys(data).forEach(function (key) {
+            var input = document.createElement("input");
+            input.type = "hidden";
             input.name = key;
             input.value = data[key];
             form.appendChild(input);
-        }
+        });
     }
-    form.style.display = 'none';
+    form.style.display = "none";
     document.body.appendChild(form);
     form.submit();
-};
+}
 
-searchGoogle = function (b64) {
-    var safeB64 = b64.replace(/\//g,'_').replace(/\+/g,'-');
+function searchGoogle(b64) {
+    "use strict";
+    var safeB64 = b64.replace(/\//g, "_").replace(/[+]/g, "-");
     var formData = {
         image_url: null,
         image_content: safeB64,
-        filename: 'cropped.png',
-        hl: 'en',
+        filename: "cropped.png",
+        hl: "en",
         encoded_image: null
     };
-    open_post('POST', 'https://www.google.com/searchbyimage/upload', formData, '_blank');
-};
+    open_post("POST", "https://www.google.com/searchbyimage/upload", formData, "_blank");
+}
 
-function startSearch(cropper){
-    var b64Text = cropper.getCroppedCanvas().toDataURL('image/png');
+function startSearch(cropper) {
+    "use strict";
+    var b64Text = cropper.getCroppedCanvas({}).toDataURL("image/png");
     //window.open(b64Text, "_blank");
-    b64Text = b64Text.replace('data:image/png;base64,','');
+    b64Text = b64Text.replace("data:image/png;base64,", "");
     searchGoogle(b64Text);
 }
 
 window.onload = function () {
-    'use strict';
+    "use strict";
 
     var Cropper = window.Cropper;
     var URL = window.URL || window.webkitURL;
-    var image = el('image');
-    var search_button = el('search_button');
+    var image = el("image");
+    var searchButton = el("search_button");
+    var inputImage = el("inputImage");
+
+    var uploadedImageURL;
+    var cropper;
 
     var options = {
         autoCrop: false,
         guides: false,
         cropend: function () {
-            if (el('search_auto').checked) {
+            if (el("search_auto").checked) {
                 startSearch(cropper);
             }
         }
     };
 
-    var cropper = new Cropper(image, options);
-    var originalImageURL = image.src;
-    var uploadedImageURL;
+    cropper = new Cropper(image, options);
 
-    // Import image
-    var inputImage = el('inputImage');
-
-    search_button.onclick = function(){
+    searchButton.onclick = function () {
         startSearch(cropper);
     };
-    
+
     if (URL) {
         inputImage.onchange = function () {
             var files = this.files;
@@ -82,18 +87,19 @@ window.onload = function () {
                         URL.revokeObjectURL(uploadedImageURL);
                     }
 
-                    image.src = uploadedImageURL = URL.createObjectURL(file);
+                    uploadedImageURL = URL.createObjectURL(file);
+                    image.src = uploadedImageURL;
                     cropper.destroy();
                     cropper = new Cropper(image, options);
                     inputImage.value = null;
                 } else {
-                    window.alert('Please choose an image file.');
+                    window.alert("Please choose an image file.");
                 }
             }
         };
     } else {
         inputImage.disabled = true;
-        inputImage.parentNode.className += ' disabled';
+        inputImage.parentNode.className += " disabled";
     }
 
     document.body.onkeydown = function (event) {
@@ -103,8 +109,8 @@ window.onload = function () {
             return;
         }
 
+        // Use http://keycode.info/ for help
         switch (e.keyCode) {
-            // Use http://keycode.info/ for help
             case 13: // Enter
                 e.preventDefault();
                 startSearch(cropper);
